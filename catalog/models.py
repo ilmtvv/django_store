bdd8c98fc51df98d4653b42eaeffa26271103a75
ilmtvv <napalkah@gmail.com>
2023-11-20 22:56:12 +0300

    сделал домашку по орм

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=64, verbose_name='имя продукта')
+    product_description = models.CharField(max_length=128, verbose_name='описание продукта')
+    product_image = models.ImageField(upload_to='images/products', verbose_name='изображение продукта', **NULLABLE)
+    product_category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='категория продукта')
+    product_price = models.FloatField(max_length=64, verbose_name='цена продукта')
+    data_of_creation = models.DateField(verbose_name='дата создания продукта')
+    data_of_modified = models.DateField(verbose_name='дата последнего изменения')
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=64, verbose_name='имя категории')
+    category_description = models.CharField(max_length=128, verbose_name='описание категории')
+    #created_at = models.CharField(max_length=4, verbose_name='test')
+
+    def __str__(self):
+        return self.category_name
+
